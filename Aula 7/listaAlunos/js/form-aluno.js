64970ebdea3c0b40e3dd3a5c52efa0fec97e0515
ALISTON MATHEUS DOS SANTOS <aliston.msantos@senacsp.edu.br>
2024-04-01 13:51:34 -0300

    Arquivos dentro da pasta XAMPP para podermos trabalhar utilizar api's

---
+++ Aula 7/listaAlunos/js/form-aluno.js
@@ -0,0 +1,72 @@
+//Obtem o formulário da pagina HTML
+var btnSalvarAluno = document.querySelector("#btnSalvarAluno");
+// Executa a função anonima ao clicar no botão
+btnSalvarAluno.addEventListener("click",
+function(event){
+    // Evita o comportamento padrão que seria recarregar a página
+    event.preventDefault();
+
+    // Obtem o formulario da nossa página HTML
+    var frmAluno = document.querySelector("#frmAluno");
+
+    //Cria um elemento tr dentro do documento HTML
+    var linhaAluno = criaLinhaAluno(frmAluno);
+
+    //Coloca o elemento tr como filha da tabela de alunos
+    var tabelaAlunos = document.querySelector("#tabela-alunos").querySelector("tbody");
+    tabelaAlunos.appendChild(linhaAluno);
+
+    frmAluno.reset();
+})
+
+function criaLinhaAluno(frmAluno) {
+    //Cria um elemento tr dentro do documento HTML
+    var linhaAluno = document.createElement("tr");
+    //coloca os elemento td como filhos do elemento tr
+    linhaAluno.appendChild(criaColuna(frmAluno.nome.value));
+    linhaAluno.appendChild(criaColuna(frmAluno.trabalho.value));
+    linhaAluno.appendChild(criaColuna(frmAluno.prova.value));
+    linhaAluno.appendChild(criaColuna(calcularMedia(frmAluno.trabalho.value, frmAluno.prova.value)));
+    return linhaAluno
+}
+
+function criaColuna(valor){
+    var coluna = document.createElement("td");
+    coluna.textContent = valor;
+    return coluna
+}
+
+
+
+    // Imprime o valor de cada campo do formulário
+    // console.log(frmAluno.nome.value);
+    // console.log(frmAluno.trabalho.value);
+    // console.log(frmAluno.prova.value);
+    // Cria um elemento tr dentro do documento HTML
+    // var linhaAluno = document.createElement("tr");
+
+    // Cria quatro elementos td dentro do documento HTML
+    // var colunaNome = document.createElement("td");
+    // var colunaTrabalho = document.createElement("td");
+    // var colunaProva = document.createElement("td");
+    // var colunaMedia = document.createElement("td");
+
+    // Coloca o conteudo correto em cada td criando anteriormente
+    // colunaNome.textContent = frmAluno.nome.value;
+    // colunaTrabalho.textContent = frmAluno.trabalho.value;
+    // colunaProva.textContent = frmAluno.prova.value;
+    // colunaMedia.textContent = calcularMedia(frmAluno.trabalho.value, frmAluno.prova.value)
+
+    //coloca os elemento td como filhos do elemento tr
+    // linhaAluno.appendChild(colunaNome);
+    // linhaAluno.appendChild(colunaTrabalho);
+    // linhaAluno.appendChild(colunaProva);
+    // linhaAluno.appendChild(colunaMedia);
+
+    //coloca o elemento tr como filha da tabela de alunos
+    // var tabelaAlunos = document.querySelector("#tabela-alunos").querySelector("tbody");
+
+    //Coloca a tr toda (ou seja ja com os TD inclusos) dentro da estrutura tbody da tabela
+    // tabelaAlunos.appendChild(linhaAluno);
+//     }
+// )
