6b5f1e15e0d6d283314605b6950557dfe43d87a2
ALISTON MATHEUS DOS SANTOS <aliston.msantos@senacsp.edu.br>
2024-05-14 16:46:18 -0300

    Criação da tela de usuarios cadastrados

---
+++ Aula 15/controller/controllerCadastro.php
@@ -0,0 +1,31 @@
+<?php
+
+    require_once('../configuracao/conexao.php');
+    require_once('../configuracao/modelCadastro.php');
+
+    $json = file_get_contents('php://input');
+    $reqbody = json_decode($json);
+    $nome = $reqbody->nomeJS;
+    $nasc = $reqbody->dataNascJS;
+    $email = $reqbody->emailJS;
+    $tel = $reqbody->telefoneJS;
+    $password = $reqbody->senhaJS;
+
+    //Criando um objeto da classe conexão;
+    $conexao = new Conexao();
+    // Criando uma variavel que ira receber o método abrirConexao atraves do obj conexao
+    $acesso = $conexao->abrirConexao();
+    // Criando um objeto ModelUsuario
+    $a = new ModelCadastro($acesso);
+
+
+    $a->nome = $nome;
+    $a->nasc = $nasc;
+    $a->email = $email;
+    $a->telefone = $tel;
+    $a->senha = $password;
+
+    
+    $retorno = $a->acessoRetorno();
+    echo json_encode($retorno);
+?>
---
+++ Aula 15/controller/controllerLogin.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('../configuracao/conexao.php');
+    require_once('../configuracao/modelUsuario.php');
+
+    //Entrada
+    $json = file_get_contents('php://input');
+    $reqbody = json_decode($json);
+    $login = $reqbody->nomeJs;
+    $senha = $reqbody->password;
+
+    //Criando um objeto da classe conexão;
+    $conexao = new Conexao();
+    // Criando uma variavel que ira receber o método abrirConexao atraves do obj conexao
+    $banco = $conexao->abrirConexao();
+    // Criando um objeto ModelUsuario
+    $m = new ModelUsuario($banco);
+
+    $m->nome = $login;
+    $m->senha = $senha;
+    $retorno = $m->entrar();
+    echo json_encode($retorno);
+?>
